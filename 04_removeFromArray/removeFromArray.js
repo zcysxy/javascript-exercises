bbc69723a23f716ebf83a3af21f0a41c538ca03b
lce <zcysxy.wyzdz@gamil.com>
2021-08-31 17:14:19 +0800

    practice04 - e1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, items) {
+    // let args = [...arguments];
+    let result = arr;
+    let removeIndex;
+    for (let i=1; i<arguments.length; i++) {
+        removeIndex = result.indexOf(arguments[i]);
+        
+        if (removeIndex !== -1) {
+            result.splice(removeIndex, 1)
+        }
+    }
+    return result;
 };
 
 module.exports = removeFromArray;
---
+++ text.js
@@ -0,0 +1,3 @@
+let a = [1,2,3,4];
+a.splice(-3,1);
+console.log(a);
