e97bbb8fde515561852e8456b4d125ddcd6aedbc
lce <zcysxy.wyzdz@gamil.com>
2021-08-31 17:21:59 +0800

    prac05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (
+        typeof int1 !== 'number' ||
+        typeof int2 !== 'number' ||
+        int1 < 0 ||
+        int2 < 0   
+    ) return 'ERROR';
 
+    return (int1 + int2) * (Math.abs(int2 - int1) + 1) / 2;
 };
 
 module.exports = sumAll;
