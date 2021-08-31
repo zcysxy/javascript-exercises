1468595692b464aa1dc461549b554887c7dc3e83
lce <zcysxy.wyzdz@gamil.com>
2021-08-31 16:44:24 +0800

    practice 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
 const reverseString = function(str) {
+    let result = '';
 
+    /*
+    // Method 1
+    for (let i=0; i<str.length; i++) {
+        result = str[i] + result;
+    }
+    */
+
+    // Method 2
+    let tempArray = str.split('');
+    tempArray.reverse();
+    result = tempArray.join('')
+
+    return result;
 };
 
 module.exports = reverseString;
